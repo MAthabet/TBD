f279750f362186ba2a909a09d843e67874d4cc3b
mohamed alaa <m-aa2001@hotmail.com>
2025-01-26 02:27:11 +0200

    player jumping handeld

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,44 @@ public class SplineMoving : MonoBehaviour
 {
     [SerializeField] private SplineContainer spline;
     [SerializeField] private float maxSpeed = 3f;
+    [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float lookAheadDistance = 0.1f;
+    [SerializeField] private float gravity = -9.81f;
 
     private float currentPos;
     float splineLength;
     float currentSpeed;
+    bool isGrounded = true;
+    CharacterController controller;
+    private Vector3 velocity;
+    private float lastYPos;
 
     void Start()
     {
         splineLength = spline.Spline.GetLength();
+        controller = GetComponent<CharacterController>();
+        lastYPos = transform.position.y;
     }
 
     private void Update()
     {
         UpdatePosOnSpline();
+        HandleGravity();
+    }
+
+    private void HandleGravity()
+    {
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+            
+            if (controller.isGrounded)
+            {
+                velocity.y = 0;
+                isGrounded = true;
+            }
+        }
     }
 
     private void UpdatePosOnSpline()
@@ -30,21 +54,35 @@ public class SplineMoving : MonoBehaviour
         
         float normalizedPos = currentPos / splineLength;
         spline.Spline.Evaluate(normalizedPos, out var pos, out var tangent, out var _);
-        pos = spline.transform.TransformPoint(pos);
+        Vector3 targetPos = spline.transform.TransformPoint(pos);
+        
+        if (!isGrounded)
+        {
+            targetPos.y = transform.position.y;
+        }
+
+        Vector3 movement = targetPos - transform.position;
+        controller.Move(movement);
+
         tangent = spline.transform.TransformVector(tangent);
         Vector3 tang = tangent;
         Vector3 lookDirection = Quaternion.Euler(30f, 0f, 0f) * tang.normalized;
-        transform.position = pos;
         transform.rotation = Quaternion.LookRotation(lookDirection);
     }
 
     void OnMove(InputValue val)
     {
-        // i flipped the input and too lazy to edit it
         currentSpeed = -val.Get<float>() * maxSpeed;
     }
+
     void OnJump()
     {
-
+        Debug.Log("Jump");
+        if (isGrounded)
+        {
+            Debug.Log("Jumping");
+            velocity.y = jumpForce;
+            isGrounded = false;
+        }
     }
 }
