f8365e4202bcce1ba3e1347c4891940d3f226587
mohamed alaa <m-aa2001@hotmail.com>
2025-02-02 14:01:39 +0200

    story scene works

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    AsyncOperation asyncOperation;
+    [SerializeField] private TextMeshProUGUI text;
+    void Start()
+    {
+        asyncOperation = SceneManager.LoadSceneAsync("Second Scene", LoadSceneMode.Additive);
+        asyncOperation.allowSceneActivation = false;
+    }
+    // Update is called once per frame
+    
+    void Update()
+    {
+        if(asyncOperation.progress >= 0.9f)
+        {
+            text.enabled = true;
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                asyncOperation.allowSceneActivation = true;
+                SceneManager.UnloadSceneAsync("Story Scene");
+
+            }
+        }
+    }
+}
